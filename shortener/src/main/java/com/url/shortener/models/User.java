3a97bb039c2cf172998a75c2f065b73571470895
Ritik Kumar <rk7382687@gmail.com>
2025-03-29 22:06:23 +0530

    complete Backend

--- shortener/src/main/java/com/url/shortener/models/User.java
+++ shortener/src/main/java/com/url/shortener/models/User.java
@@ -1,4 +1,18 @@
 package com.url.shortener.models;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String username;
+    private String password;
+    private String role = "ROLE_USER";
 }
